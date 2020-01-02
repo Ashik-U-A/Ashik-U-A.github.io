5c5a82b27564a700f3b63856b671d30019aaecc6
Ashik U A <ashik.unni.00@gmail.com>
2020-01-01 22:56:27 -0700

    Simple strealined Rendering Logic.

--- scripts/world-creator.js
+++ scripts/world-creator.js
@@ -1,3 +1,44 @@
+/**
+ * This Section of the code is the Core of 3D Rendering.
+ * It contains the logic to streamline the rendering.
+ */
+
+let pre_rendering_queue = [];
+let rendering_tasks = [];
+
+function dequeue_rendering_queue() {
+    rendering_tasks.forEach((task, i) => {
+        task();
+        delete rendering_tasks[i];
+    });
+    rendering_tasks = [];
+}
+
+function enqueue_rendering_queue() {
+    pre_rendering_queue.forEach(task => {
+        rendering_tasks.push(task);
+    });
+}
+
+function add_to_pre_rendering_queue(action) {
+    return pre_rendering_queue.push(action) - 1;
+}
+
+function remove_from_pre_rendering_queue(index) {
+    pre_rendering_queue.splice(index, 1);
+}
+
+function start_rendering() {
+    requestAnimationFrame(start_rendering);
+    dequeue_rendering_queue();
+    enqueue_rendering_queue();
+    world.renderer.render(world.scene, world.camera);
+}
+
+/**
+ * This Section of the code deals with the Content and Interaction with the 3D world.
+ */
+
 const world = {
     camera: null,
     scene: null,
@@ -26,12 +67,12 @@ function create_first_world() {
 
     world.camera.position.z = 5;
 
-    start_rendering();
-}
+    add_to_pre_rendering_queue(() => {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+    });
 
-function start_rendering() {
-    requestAnimationFrame(start_rendering);
-    world.renderer.render(world.scene, world.camera);
+    start_rendering();
 }
 
 export const world_creator = {
